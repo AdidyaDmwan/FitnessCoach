bd6c7f895d45ce385feeaebd50636a9f7f18c627
Adidya <Adidyadarmawan12@gmail.com>
2026-05-13 17:25:52 +0800

    Initial Commit

---
+++ FitnessCoach/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FitnessCoach
+//
+//  Created by 17 on 2026/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessCoach/FitnessCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessCoachApp.swift
+//  FitnessCoach
+//
+//  Created by 17 on 2026/5/13.
+//
+
+import SwiftUI
+
+@main
+struct FitnessCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
